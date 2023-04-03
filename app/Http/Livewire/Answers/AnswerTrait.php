cd0ef458bf933597c3068035ebe54e8cf0bc034c
Osama Sa'dah <osama.sada98@gmail.com>
2023-04-04 01:22:42 +0300

    answers

--- app/Http/Livewire/Answers/AnswerTrait.php
+++ app/Http/Livewire/Answers/AnswerTrait.php
@@ -3,7 +3,7 @@
 namespace App\Http\Livewire\Answers;
 
 use App\Business\Livewire\WithEvents;
-use App\Business\Models\Medias;
+
 use App\Models\Answer;
 use Livewire\WithFileUploads;
 
@@ -17,29 +17,5 @@ trait AnswerTrait
     protected function rules(): array
     {
         return ['answer.content' => 'required'];
-    }
-
-    public function save()
-    {
-        $this->validate();
-        $exists = $this->answer->exists;
-
-        $this->answer->save();
-        Medias::addMedia($this->answer, $this->files);
-
-        if ($exists) {
-            $this->edited('Answer');
-            $this->emitTo('media.index', 'refreshMedia');
-            return;
-        }
-
-        $this->created('Answer');
-
-        if ($this->answer->question) {
-            $this->redirect(route('answers.index', ['question' => $this->answer->question->id]));
-            return;
-        }
-
-        $this->redirect(route('answers.edit', ['answer' => $this->answer->id]));
     }
 }
--- app/Http/Livewire/Answers/Edit.php
+++ app/Http/Livewire/Answers/Edit.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Livewire\Answers;
 
+use App\Business\Models\Medias;
 use App\Models\Answer;
 use Livewire\Component;
 
@@ -17,5 +18,16 @@ class Edit extends Component
     public function render()
     {
         return view('livewire.answers.edit');
+    }
+
+    public function save()
+    {
+        $this->validate();
+
+        $this->answer->save();
+        Medias::addMedia($this->answer, $this->files);
+
+        $this->edited('Answer');
+        $this->emitTo('media.index', 'refreshMedia');
     }
 }
