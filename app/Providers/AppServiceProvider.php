5c5de89e623c40501d2d8e534ad72bcf31f70648
Osama Sa'dah <osama.sada98@gmail.com>
2023-05-13 23:35:41 +0300

    force https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -12,7 +12,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        if($this->app->environment('production')) {
+            $this->app['request']->server->set('HTTPS', true);
+        }
     }
 
     /**
@@ -20,8 +22,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if($this->app->environment('production')) {
-            URL::forceScheme('https');
-        }
+        //
     }
 }
